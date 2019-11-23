e042a5dbf39b836a5c30eccbb6e7e583ea4bf407
David Graat <david.graat@aurora.org>
2019-11-23 00:20:53 -0600

    Add files

---
+++ SettingsReader/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SettingsReader.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
